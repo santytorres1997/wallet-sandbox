71da7fda71f5ee62f3649ae487c56aeb7dc0b449
santytorres1997 <santytorres192@gmail.com>
2026-04-12 16:27:02 -0300

    Preparo backend para deploy

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ const accountRoutes = require('./routes/account.routes.js');
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
